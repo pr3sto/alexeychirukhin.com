6fd5167a74b4af80a1cf6dcd1c2e83933c759220
Alexey Chirukhin <pr3sto1377@gmail.com>
2022-07-01 03:16:22 +0400

    add transition to side panel appearence

--- components/SidePanel.vue
+++ components/SidePanel.vue
@@ -1,12 +1,22 @@
 <template>
   <aside class="side-panel">
-    <p>{{ text }}</p>
+    <transition name="opacity-transition">
+      <p :key="textHash">{{ text }}</p>
+    </transition>
   </aside>
 </template>
 
 <style scoped>
 .side-panel {
   writing-mode: vertical-lr;
+}
+
+/* transition opacity */
+.opacity-transition-enter-active {
+  transition: opacity 0.2s cubic-bezier(0.8, 0.2, 0.1, 0.8);
+}
+.opacity-transition-enter {
+  opacity: 0;
 }
 
 @media only screen and (max-width: 700px) {
@@ -29,6 +39,12 @@ export default {
       } else {
         return page.menuItemText;
       }
+    },
+    textHash() {
+      return this.text.split("").reduce(function (a, b) {
+        a = (a << 5) - a + b.charCodeAt(0);
+        return a & a;
+      }, 0);
     },
   },
 };
