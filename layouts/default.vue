62e5863013e989120bc79e998bd6d8bec8d013f9
Alexey Chirukhin <pr3sto1377@gmail.com>
2023-07-08 01:50:33 +0400

    add page transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,7 @@
 .layout {
   color: var(vars.$page-styles__font-color);
   background: var(vars.$page-styles__background-color);
+  transition: background linear 0.8s;
 }
 
 .grain {
@@ -30,6 +31,21 @@
 .overlay {
   position: absolute;
   z-index: 2;
+}
+
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-leave-to {
+  opacity: 0;
+}
+
+.page-enter-active {
+  transition: filter 1.2s, opacity 1s;
+}
+.page-enter {
+  opacity: 0;
+  filter: blur(15px) brightness(20);
 }
 </style>
 
