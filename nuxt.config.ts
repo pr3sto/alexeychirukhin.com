718cb2fc8840f47929017b73df56159d2e33b507
Alexey Chirukhin <pr3sto1377@gmail.com>
2023-08-20 23:07:53 +0400

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,45 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true
-})
+  ssr: false,
+  app: {
+    head: {
+      title: "Alexey Chirukhin",
+      titleTemplate(title) {
+        return title && title !== "Alexey Chirukhin"
+          ? `${title} — Alexey Chirukhin`
+          : "Alexey Chirukhin";
+      },
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Personal Website of Alexey Chirukhin",
+        },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        /* Fonts */
+        { rel: "preconnect", href: "https://fonts.cdnfonts.com" },
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600&family=Chango&family=Schoolbell&family=Just+Me+Again+Down+Here&display=swap",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.cdnfonts.com/css/violet-sans",
+        },
+      ],
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+});
