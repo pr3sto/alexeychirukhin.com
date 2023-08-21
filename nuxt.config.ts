cddeee6896f485441627bd10b6da2504c5f82b0a
Alexey Chirukhin <pr3sto1377@gmail.com>
2023-08-21 23:17:59 +0400

    add scss to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,4 +49,14 @@ export default defineNuxtConfig({
       },
     },
   },
+  css: ["@/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
+  },
 });
