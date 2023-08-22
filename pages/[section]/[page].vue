1f92f342e46218c3e65210d95c2cf93f9888d9ef
Alexey Chirukhin <pr3sto1377@gmail.com>
2023-08-22 22:53:16 +0400

    add pages

---
+++ pages/[section]/[page].vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>Route: {{ route.path }}</h1>
+    <h2>Section: {{ route.params.section }}; page: {{ route.params.page }};</h2>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1>Route: {{ route.path }}</h1>
+  </div>
+</template>
