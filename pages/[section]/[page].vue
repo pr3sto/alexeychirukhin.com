04fd3bba6d4846421e08436511293dbe45956194
Alexey Chirukhin <pr3sto1377@gmail.com>
2023-08-25 22:12:23 +0400

    call page api from pages

--- pages/[section]/[page].vue
+++ pages/[section]/[page].vue
@@ -1,10 +1,38 @@
 <script lang="ts" setup>
+import { useMenuStore } from "~/stores/menu";
+import PageService from "~/app/services/PageService";
+
 const route = useRoute();
+const config = useRuntimeConfig();
+const menuStore = useMenuStore();
+
+const menuPage = menuStore.getSectionMenuPage(
+  route.params.section as string,
+  route.params.page as string,
+);
+
+if (!menuPage) {
+  throw createError({ statusCode: 404, statusMessage: "Page Not Found" });
+}
+
+useHead({
+  title: menuPage?.displayName,
+});
+
+const { pending, data } = await useLazyAsyncData(
+  `page${menuPage.id}`,
+  async () => {
+    const pageData = await PageService.getAsync(
+      config.public.PAGE_API_URL,
+      menuPage.id,
+    );
+    return pageData;
+  },
+);
 </script>
 
 <template>
   <div>
-    <h1>Route: {{ route.path }}</h1>
-    <h2>Section: {{ route.params.section }}; page: {{ route.params.page }};</h2>
+    <span>{{ data }}</span>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <script lang="ts" setup>
-const route = useRoute();
+import { useMenuStore } from "~/stores/menu";
+import PageService from "~/app/services/PageService";
+
+const config = useRuntimeConfig();
+const menuStore = useMenuStore();
+
+const menuPage = menuStore.indexMenuPage;
+
+if (!menuPage) {
+  throw createError({ statusCode: 404, statusMessage: "Page Not Found" });
+}
+
+const { pending, data } = await useLazyAsyncData(
+  `page${menuPage.id}`,
+  async () => {
+    const pageData = await PageService.getAsync(
+      config.public.PAGE_API_URL,
+      menuPage.id,
+    );
+    return pageData;
+  },
+);
 </script>
 
 <template>
   <div>
-    <h1>Route: {{ route.path }}</h1>
+    <span>{{ data }}</span>
   </div>
 </template>
