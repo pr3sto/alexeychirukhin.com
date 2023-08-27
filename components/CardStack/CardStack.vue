6333e862a6fd744d80853469d729986e8a58eff2
Alexey Chirukhin <pr3sto1377@gmail.com>
2023-08-28 01:01:47 +0400

    add CardStack component prototype

---
+++ components/CardStack/CardStack.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { CardStack } from "~/app/types/page";
+
+defineProps({
+  content: {
+    type: Object as PropType<CardStack>,
+    required: true,
+  },
+});
+
+const { isMobileScreen } = useScreenDetection();
+</script>
+
+<template>
+  <CardStackSm v-if="isMobileScreen" :content="content" />
+  <CardStackLg v-else :content="content" />
+</template>
---
+++ components/CardStack/CardStackLg.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { CardStack } from "~/app/types/page";
+
+defineProps({
+  content: {
+    type: Object as PropType<CardStack>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <span>{{ content }}</span>
+</template>
