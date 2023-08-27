9a8c84325d53b246108f6a0063b15947ec7eaf77
Alexey Chirukhin <pr3sto1377@gmail.com>
2023-08-27 17:45:33 +0400

    add cardstack constants

---
+++ constants/cardstack.ts
@@ -0,0 +1,5 @@
+export const CARD_ASPECT_RATIO = 0.83;
+export const FONT_SIZE_FACTOR1 = 45; // magic number
+export const FONT_SIZE_FACTOR2 = 37; // magic number
+export const CARD_MAX_ROTATE_ANGLE_DEG = 7;
+export const CARD_CONTAINER_PADDING_PERCENT = 0.1;
